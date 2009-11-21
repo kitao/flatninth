c53f408c351fb76f2a3fd01b529dd32df99dffc7
takashi.kitao <takashi.kitao@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2009-11-21 12:49:32 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@3 7bb76224-d653-11de-9196-2db6e9c7c7ae

--- test/id_test.js
+++ test/id_test.js
@@ -20,45 +20,20 @@
  * THE SOFTWARE.
  */
 
-b9.assert = function (test)
 {
-    if (!test)
-    {
-        alert("assertion failed");
-    }
-};
+    var id1 = new b9.ID("abc");
+    var id2 = new b9.ID("abc");
+    var id3 = new b9.ID("def");
 
+    b9.assert(id1.isEqual(id2));
+    b9.assert(!id2.isEqual(id3));
+}
 
-b9.include("id_test.js");
-
-
-alert("complete test");
-
-/*
-function draw()
 {
-    var canvas = document.getElementById('canvassample');
-    var ctx = canvas.getContext('2d');
+    var id1 = new b9.ID();
+    var id2 = new b9.ID();
 
-    ctx.clearRect(0, 0, 200, 200);
+    b9.assert(!id1.isEqual(id2));
+}
 
-    ctx.beginPath();
-    ctx.moveTo(x, x);
-    ctx.lineTo(120, 20);
-    ctx.lineTo(120, 120);
-    ctx.lineTo(20, 120);
-    ctx.closePath();
-    ctx.stroke();
-
-    x += 10;
-
-    clearTimeout(timer);
-    timer = setTimeout(draw, 1000);
-};
-
-var x = 0;
-var interval = 500;
-var timer;
-
-draw();
-*/
+alert("id_test");
