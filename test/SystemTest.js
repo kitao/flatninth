591ec6b158c0fd2a398ab70443a39c4d8b244a73
takashi.kitao <takashi.kitao@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2010-06-19 10:07:45 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@250 7bb76224-d653-11de-9196-2db6e9c7c7ae

--- test/SystemTest.js
+++ test/SystemTest.js
@@ -25,4 +25,8 @@ function testSystem() {
     b9.System.setup("test_canvas", 60);
 
     // TODO
+    //
+    b9.System.defaultTask(b9.System.ORDER_NORMAL);
+
+    b9.System.defaultView(b9.System.ORDER_NORMAL);
 }
