4321347a303d98dc5519c67d49ea196237595f73
takashi.kitao <takashi.kitao@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2010-08-22 09:03:34 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@310 7bb76224-d653-11de-9196-2db6e9c7c7ae

--- source/Sprite.js
+++ source/Sprite.js
@@ -121,10 +121,11 @@ b9.Sprite.prototype.rectUV = function(rect_index) {
     return this._rect[rect_index].uv;
 };
 
-b9.Sprite.prototype._render = function(canvas_proxy) {
+b9.Sprite.prototype._render = function(canvas) {
     this._calcFinal();
 
-    var image = b9.Asset.getAsset(this._image);
+    var context = canvas.context;
+    var image = b9.Asset.getImage(this._image);
 
     for (var i = 0; i < this._cur_rect_num; i++) {
         var rect = this._rect[i];
@@ -142,10 +143,11 @@ b9.Sprite.prototype._render = function(canvas_proxy) {
         if (image) {
             if (image.is_ready) {
                 // TODO: branch by UV
-                b9.LowLevelAPI.drawImage(canvas_proxy, image, x, y, w, h);
+                context.drawImage(image, x, y, w, h);
             }
         } else {
-            b9.LowLevelAPI.fillRect(canvas_proxy, x, y, w, h, b9.Sprite._color1.toRGBA());
+            context.fillStyle = b9.Sprite._color1.toRGBA();
+            context.fillRect(x, y, w, h);
         }
     }
 };
