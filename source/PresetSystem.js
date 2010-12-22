9277b901bf0c93dfbddf1dcb993c17187314686c
takashi.kitao@gmail.com <takashi.kitao@gmail.com@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2010-12-22 02:05:29 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@479 7bb76224-d653-11de-9196-2db6e9c7c7ae

--- source/PresetSystem.js
+++ source/PresetSystem.js
@@ -28,31 +28,158 @@ b9.PresetSystem = {};
 /**
  *
  */
-b9.PresetSystem.setup = function(canvas_id, max_alist_range, max_scr_range, target_fps) {
-    b9.System.setup(canvas_id, max_alist_range, max_scr_range, target_fps);
+b9.PresetSystem.setup = function(canvas_id, target_fps) {
+    var i;
+    var canvas_width, canvas_height;
 
-    var canvas_width = b9.System.getCanvasWidth();
-    var canvas_height = b9.System.getCanvasHeight();
+    b9.System.setup(canvas_id, this._updateFunc, this._renderFunc, target_fps);
 
-    this._max_alist_range = max_alist_range;
-    this._max_scr_range = max_scr_range;
+    canvas_width = b9.System.getFramebufferWidth();
+    canvas_height = b9.System.getFramebufferHeight();
 
-    this._alist_array = new Array(max_alist_range * 2 + 1);
-    this._scr_array = new Array(max_scr_range * 2 + 1);
+    this._first_al = new b9.ActorList();
+    this._prev_al = new b9.ActorList();
+    this._normal_al = new b9.ActorList();
+    this._next_al = new b9.ActorList();
+    this._last_al = new b9.ActorList();
 
-    // TODO
+    this._first_scr = new b9.Screen(canvas_width, canvas_height);
+    this._prev_scr = new b9.Screen(canvas_width, canvas_height);
+    this._normal_scr = new b9.Screen(canvas_width, canvas_height);
+    this._next_scr = new b9.Screen(canvas_width, canvas_height);
+    this._last_scr = new b9.Screen(canvas_width, canvas_height);
+
+    this._first_draw = new b9.Drawable();
+    this._prev_draw = new b9.Drawable();
+    this._normal_draw = new b9.Drawable();
+    this._next_draw = new b9.Drawable();
+    this._last_draw = new b9.Drawable();
 };
 
 /**
- *
+ * Returns the actor list which gets updated first.
+ * @return {b9.ActorList} The first actor list.
  */
+b9.PresetSystem.getFirstActorList = function() {
+    return this._first_al;
+};
+
+/**
+ * Returns the actor list which gets updated second.
+ * @return {b9.ActorList} The prevous actor list.
+ */
+b9.PresetSystem.getPrevActorList = function() {
+    return this._prev_al;
+};
+
+/**
+ * Returns the actor list which gets updated third.
+ * @return {b9.ActorList} The normal actor list.
+ */
+b9.PresetSystem.getNormalActorList = function() {
+    return this._normal_al;
+};
+
+/**
+ * Returns the actor list which gets updated fourth.
+ * @return {b9.ActorList} The next actor list.
+ */
+b9.PresetSystem.getNextActorList = function() {
+    return this._next_al;
+};
+
+/**
+ * Returns the actor list which gets updated last.
+ * @return {b9.ActorList} The last actor list.
+ */
+b9.PresetSystem.getLastActorList = function() {
+    return this._last_al;
+};
+
+/**
+ * Returns the screen which gets rendered first.
+ * @return {b9.Screen} The first screen.
+ */
+b9.PresetSystem.getFirstScreen = function() {
+    return this._first_scr;
+};
+
+/**
+ * Returns the screen which gets rendered second.
+ * @return {b9.Screen} The previous screen.
+ */
+b9.PresetSystem.getPrevScreen = function() {
+    return this._prev_scr;
+};
+
+/**
+ * Returns the screen which gets rendered third.
+ * @return {b9.Screen} The normal screen.
+ */
+b9.PresetSystem.getNormalScreen = function() {
+    return this._normal_scr;
+};
+
+/**
+ * Returns the screen which gets rendered fourth.
+ * @return {b9.Screen} The next screen.
+ */
+b9.PresetSystem.getNextScreen = function() {
+    return this._next_scr;
+};
+
+/**
+ * Returns the screen which gets rendered last.
+ * @return {b9.Screen} The last screen.
+ */
+b9.PresetSystem.getLastScreen = function() {
+    return this._last_scr;
+};
+
+/**
+ * Returns the root drawable of the first screen.
+ * @return {b9.Drawable} The root drawable of the first screen.
+ */
+b9.PresetSystem.getFirstRootDrawable = function() {
+    return this._first_draw;
+};
+
+/**
+ * Returns the root drawable of the previous screen.
+ * @return {b9.Drawable} The root drawable of the previous screen.
+ */
+b9.PresetSystem.getPrevRootDrawable = function() {
+    return this._prev_draw;
+};
+
+/**
+ * Returns the root drawable of the normal screen.
+ * @return {b9.Drawable} The root drawable of the normal screen.
+ */
+b9.PresetSystem.getNormalRootDrawable = function() {
+    return this._normal_draw;
+};
+
+/**
+ * Returns the root drawable of the next screen.
+ * @return {b9.Drawable} The root drawable of the next screen.
+ */
+b9.PresetSystem.getNextRootDrawable = function() {
+    return this._next_draw;
+};
+
+/**
+ * Returns the root drawable of the last screen.
+ * @return {b9.Drawable} The root drawable of the last screen.
+ */
+b9.PresetSystem.getLastRootDrawable = function() {
+    return this._last_draw;
+};
+
 b9.PresetSystem._updateFunc = function() {
     // TODO
 };
 
-/**
- *
- */
 b9.PresetSystem._renderFunc = function() {
     // TODO
 };
