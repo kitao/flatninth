58c69c0f1a7dc27bc362be7d22a41176b198659d
takashi.kitao@gmail.com <takashi.kitao@gmail.com@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2010-12-30 06:59:42 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@500 7bb76224-d653-11de-9196-2db6e9c7c7ae

--- source/Shader.js
+++ source/Shader.js
@@ -93,7 +93,7 @@ b9.Shader.prototype._setup = function() {
         gl.deleteShader(frag_glshd);
 
         this._local_to_screen_loc = gl.getUniformLocation(this._glprog, "b9_local_to_screen");
-        this._vertex_loc = gl.getAttribLocation(this._glprog, "b9_vertex");
+        this._pos_loc = gl.getAttribLocation(this._glprog, "b9_pos");
         this._color_loc = gl.getAttribLocation(this._glprog, "b9_color");
         this._texcoord_loc = gl.getAttribLocation(this._glprog, "b9_texcoord");
 
