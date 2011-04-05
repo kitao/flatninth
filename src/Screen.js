0e0852fb94e1ecc180ef4091625333473ff41ade
takashi.kitao@gmail.com <takashi.kitao@gmail.com@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2011-04-05 14:08:37 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@681 7bb76224-d653-11de-9196-2db6e9c7c7ae

--- src/Screen.js
+++ src/Screen.js
@@ -34,187 +34,86 @@ b9.Screen = b9.createClass();
  * @ignore
  */
 b9.Screen.prototype.initialize = function(width, height) {
-    this.scrFlag_ = b9.Screen.ScreenFlag.VISIBLE;
-    this.x_ = 0;
-    this.y_ = 0;
-    this.width_ = width;
-    this.height_ = height;
-    this.clearColor_ = new b9.Color(255, 0, 0, 255);
-    this.focalLength_ = 1000.0;
-    this.nearClipDist_ = 10.0;
-    this.farClipDist_ = 100000.0;
-    this.innerScaleX_ = 1.0;
-    this.innerScaleY_ = 1.0;
-    this.camera_ = new b9.Matrix3D(b9.Matrix3D.UNIT);
-    this.camera_.trans.x = this.focalLength_;
+    /**
+     * @return {b9.ScreenFlag}
+     */
+    this.screenFlag = b9.ScreenFlag.VISIBLE;
 
-    this.cameraToScreenArray_ = [];
+    /**
+     * The left position in the canvas.
+     * @return {Number}
+     */
+    this.x = 0;
+
+    /**
+     * The top position in the canvas.
+     * @return {Number}
+     */
+    this.y = 0;
+
+    /**
+     * The width of this screen.
+     * @return {Number}
+     */
+    this.width = width;
+
+    /**
+     * The height of this screen.
+     * @return {Number}
+     */
+    this.height = height;
+
+    /**
+     * The clear color of this screen.
+     * @return {b9.Color}
+     */
+    this.clearColor = new b9.Color(255, 0, 0, 255);
+
+    /**
+     * The focal length of this screen.
+     * @return {Number}
+     */
+    this.focalLength = 1000.0;
+
+    /**
+     * The near clip distance of this screen.
+     * @return {Number}
+     */
+    this.nearClipDist = 10.0;
+
+    /**
+     * The far clip distance of thie screen.
+     * @return {Number}
+     */
+    this.farClipDist = 100000.0;
+
+    /**
+     *
+     * @return {Number}
+     */
+    this.innerScaleX = 1.0;
+
+    /**
+     *
+     * @return {Number}
+     */
+    this.innerScaleY = 1.0;
+
+    /**
+     * The camera matrix of this screen.
+     * @return {b9.Matrix3D}
+     */
+    this.camera = new b9.Matrix3D(b9.Matrix3D.UNIT);
+    this.camera.trans.x = this.focalLength;
+
+    this._cameraToScreenArray = [];
 };
 
 /**
- * hoge
+ *
  */
 b9.Screen.prototype.finalize = function() {
     // TODO
-};
-
-
-/**
- * Returns whether the specified screen flag is enabled.
- * @param {b9.Screen.ScreenFlag} scrFlag A screen flag.
- * @return {Boolean} true the flag is enabled; false otherwise.
- */
-b9.Screen.prototype.getScreenFlag = function(scrFlag) {
-    return (this.scrFlag_ & scrFlag) ? true : false;
-};
-
-/**
- * Sets the specified screen flag.
- * @param {b9.Screen.ScreenFlag} scrFlag A screen flag.
- * @param {Boolean} isEnabled Whether the flag is enabled.
- */
-b9.Screen.prototype.setScreenFlag = function(scrFlag, isEnabled) {
-    if (isEnabled) {
-        this.scrFlag_ |= scrFlag;
-    } else {
-        this.scrFlag_ &= ~scrFlag;
-    }
-};
-
-/**
- * Returns the left position in the canvas.
- * @return {Number} The left position.
- */
-b9.Screen.prototype.getX = function() {
-    return this.x_;
-};
-
-/**
- * Returns the top position in the canvas.
- * @return {Number} The top position.
- */
-b9.Screen.prototype.getY = function() {
-    return this.y_;
-};
-
-/**
- * Sets the position in the canvas.
- * @param {Number} x A left position.
- * @param {Number} y A top position.
- */
-b9.Screen.prototype.setPos = function(x, y) {
-    this.x_ = x;
-    this.y_ = y;
-};
-
-/**
- * Returns the width of this screen.
- * @return {Number} The width of this screen.
- */
-b9.Screen.prototype.getWidth = function() {
-    return this.width_;
-};
-
-/**
- * Returns the height of this screen.
- * @return {Number} The height of this screen.
- */
-b9.Screen.prototype.getHeight = function() {
-    return this.height_;
-};
-
-/**
- * Sets the size of this screen.
- * @param {Number} width The width of a screen.
- * @param {Number} height The height of a screen.
- */
-b9.Screen.prototype.setSize = function(width, height) {
-    this.width_ = width;
-    this.height_ = height;
-};
-
-/**
- * Returns the clear color of this screen.
- * @return {b9.Color} The clear color.
- */
-b9.Screen.prototype.getClearColor = function() {
-    return this.clearColor_;
-};
-
-/**
- * Returns the focal length of this screen.
- * @return {Number} The focal length.
- */
-b9.Screen.prototype.getFocalLength = function() {
-    return this.focalLength_;
-};
-
-/**
- * Sets the focal length of this screen.
- * @param {Number} focal_length A focal length.
- */
-b9.Screen.prototype.setFocalLength = function(focal_length) {
-    this.focalLength_ = focal_length;
-};
-
-/**
- * Returns the near clip distance of this screen.
- * @return {Number} The near clip distance.
- */
-b9.Screen.prototype.getNearClipDist = function() {
-    return this.nearClipDist_;
-};
-
-/**
- * Returns the far clip distance of thie screen.
- * @return {Number} The far clip distance.
- */
-b9.Screen.prototype.getFarClipDist = function() {
-    return this.farClipDist_;
-};
-
-/**
- * Sets the clip distances of this screen.
- * @param {Number} nearClipDist A near clip distance.
- * @param {Number} farClipDist A far clip distance.
- */
-b9.Screen.prototype.setClipDist = function(nearClipDist, farClipDist) {
-    this.nearClipDist_ = nearClipDist;
-    this.farClipDist_ = farClipDist;
-};
-
-/**
- *
- * @return {Number}
- */
-b9.Screen.prototype.getInnerScaleX = function() {
-    return this.innerScaleY_;
-};
-
-/**
- *
- * @return {Number}
- */
-b9.Screen.prototype.getInnerScaleY = function() {
-    return this.innerScaleY_;
-};
-
-/**
- *
- * @param {Number} scaleX
- * @param {Number} scaleY
- */
-b9.Screen.prototype.setInnerScale = function(scaleX, scaleY) {
-    this.innerScaleX_ = scaleX;
-    this.innerScaleY_ = scaleY;
-};
-
-/**
- * Returns the camera matrix of this screen.
- * @return {b9.Matrix3D} The camera matrix.
- */
-b9.Screen.prototype.getCamera = function() {
-    return this.camera_;
 };
 
 /**
@@ -223,38 +122,38 @@ b9.Screen.prototype.getCamera = function() {
  */
 b9.Screen.prototype.render = function(rootNode) {
     var node;
-    var gl = b9.System.getGLContext();
-    var camera = this.camera_;
+    var gl = b9.gl;
+    var ScreenFlag = b9.ScreenFlag;
+    var NodeFlag = b9.NodeFlag;
+
+    var camera = this.camera;
     var clearFlag = 0;
     var sortList = null;
 
-    var ScreenFlag = b9.Screen.ScreenFlag;
-    var NodeFlag = b9.Node.NodeFlag;
-
-    var worldToCamera = b9.Screen.mat1_;
+    var worldToCamera = b9.Screen._mat1;
 
     var worldToCameraArray = [];
     var worldToScreenArray = [];
 
-    this.updateCameraToScreen_(); // TODO
+    this._updateCameraToScreen(); // TODO
 
     worldToCamera.set(b9.Matrix3D.UNIT).toLocal(camera);
     worldToCamera.toArray(worldToCameraArray);
 
-    b9.Matrix3D.mulArray(this.cameraToScreenArray_, worldToCameraArray, worldToScreenArray);
+    b9.Matrix3D.mulArray(this._cameraToScreenArray, worldToCameraArray, worldToScreenArray);
 
     // gl.viewport(x, y, w, h);
 
-    if (this.getScreenFlag(ScreenFlag.CLEAR_COLOR)) {
+    if (this.screenFlag & ScreenFlag.CLEAR_COLOR) {
         gl.clearColor(
-                this.clearColor_.getR() / 255.0,
-                this.clearColor_.getG() / 255.0,
-                this.clearColor_.getB() / 255.0,
-                this.clearColor_.getA() / 255.0);
+                this.clearColor.r / 255.0,
+                this.clearColor.g / 255.0,
+                this.clearColor.b / 255.0,
+                this.clearColor.a / 255.0);
         clearFlag = gl.COLOR_BUFFER_BIT;
     }
 
-    if (this.getScreenFlag(ScreenFlag.CLEAR_DEPTH)) {
+    if (this.screenFlag & ScreenFlag.CLEAR_DEPTH) {
         clearFlag |= gl.DEPTH_BUFFER_BIT;
     }
 
@@ -262,16 +161,15 @@ b9.Screen.prototype.render = function(rootNode) {
         gl.clear(clearFlag);
     }
 
-    for (node = rootNode; node; node = node.getNextAsList()) {
-        if (node.getNodeFlag(NodeFlag.VISIBLE)) {
-            if (node.getNodeFlag(NodeFlag.Z_SORT)) {
-                node.sortValue_ =
-                    b9.Screen.vec1_.set(node._world.trans).sub(camera.trans).dot(camera.zAxis);
+    for (node = rootNode; node; node = node.nextAsList) {
+        if (node.nodeFlag & NodeFlag.VISIBLE) {
+            if (node.nodeFlag & NodeFlag.Z_SORT) {
+                node._sortValue = b9.Screen._vec1.set(node._world.trans).sub(camera.trans).dot(camera.zAxis);
 
-                node.sortNext_ = sortList;
+                node._sortNext = sortList;
                 sortList = node;
             } else {
-                node.draw_(worldToScreenArray);
+                node._draw(worldToScreenArray);
             }
         } else {
             node = node.getLastDescendant();
@@ -279,10 +177,10 @@ b9.Screen.prototype.render = function(rootNode) {
     }
 
     if (sortList) {
-        sortList = b9.Screen.sortList_(sortList, null, null);
+        sortList = b9.Screen._sortList(sortList, null, null);
 
-        for (node = sortList; node; node = node.sortNext_) {
-            node.draw_(worldToScreenArray);
+        for (node = sortList; node; node = node._sortNext) {
+            node._draw(worldToScreenArray);
         }
     }
 };
@@ -294,24 +192,24 @@ b9.Screen.prototype.dump = function() {
     // TODO
 };
 
-b9.Screen.prototype.updateCameraToScreen_ = function() {
-    var array = this.cameraToScreenArray_;
-    var invSub = 1.0 / (this.farClipDist_ - this.nearClipDist_);
+b9.Screen.prototype._updateCameraToScreen = function() {
+    var array = this._cameraToScreenArray;
+    var invSub = 1.0 / (this.farClipDist - this.nearClipDist);
 
-    array[0] = this.focalLength_ * 2.0 / this.width_;
+    array[0] = this.focalLength * 2.0 / this.width;
     array[4] = 0.0;
     array[8] = 0.0;
     array[12] = 0.0;
 
     array[1] = 0.0;
-    array[5] = this.focalLength_ * 2.0 / this.height_;
+    array[5] = this.focalLength * 2.0 / this.height;
     array[9] = 0.0;
     array[13] = 0.0;
 
     array[2] = 0.0;
     array[6] = 0.0;
-    array[10] = (this.farClipDist_ + this.nearClipDist_) * invSub;
-    array[14] = 2.0 * this.farClipDist_ * this.nearClipDist_ * invSub;
+    array[10] = (this.farClipDist + this.nearClipDist) * invSub;
+    array[14] = 2.0 * this.farClipDist * this.nearClipDist * invSub;
 
     array[3] = 0.0;
     array[7] = 0.0;
@@ -319,21 +217,21 @@ b9.Screen.prototype.updateCameraToScreen_ = function() {
     array[15] = 0.0;
 };
 
-b9.Screen.sortList_ = function(sortList, start, end) {
+b9.Screen._sortList = function(sortList, start, end) {
     var node, next;
     var center = sortList;
-    var centerSortValue = center.sort_value;
+    var centerSortValue = center._sortValue;
     var left = null;
     var leftEnd = null;
     var right = null;
     var rightEnd = null;
 
-    sortList = sortList.sortNext_;
+    sortList = sortList._sortNext;
 
     for (node = sortList; node != end; node = next) {
-        next = node.sortNext_;
+        next = node._sortNext;
 
-        if (node.sort_value <= centerSortValue) {
+        if (node._sortValue <= centerSortValue) {
             if (!left) {
                 leftEnd = node;
             }
@@ -352,35 +250,34 @@ b9.Screen.sortList_ = function(sortList, start, end) {
 
     if (left) {
         if (start) {
-            start.sortNext_ = left;
+            start._sortNext = left;
         }
 
-        leftEnd.sortNext_ = center;
-        sortList = b9.Screen.sortList_(left, start, center);
+        leftEnd._sortNext = center;
+        sortList = b9.Screen._sortList(left, start, center);
     } else {
         if (start) {
-            start.sortNext_ = center;
+            start._sortNext = center;
         }
 
         sortList = center;
     }
 
     if (right) {
-        center.sortNext_ = right;
-        rightEnd.sortNext_ = end;
-        b9.Screen.sortList_(right, center, end);
+        center._sortNext = right;
+        rightEnd._sortNext = end;
+        b9.Screen._sortList(right, center, end);
     } else {
-        center.sortNext_ = end;
+        center._sortNext = end;
     }
 
     return sortList;
 };
 
 /**
- * The flags which specify how to draw a screen.
- * @enum {Number}
+ * @class The flags which specify how to draw a screen.
  */
-b9.Screen.ScreenFlag = {
+b9.ScreenFlag = {
     /**
      * TODO
      * @constant
@@ -403,6 +300,5 @@ b9.Screen.ScreenFlag = {
     CLEAR_DEPTH: 0x2000
 };
 
-b9.Screen.vec1_ = new b9.Vector3D();
-b9.Screen.mat1_ = new b9.Matrix3D();
-b9.Screen.mat2_ = new b9.Matrix3D();
+b9.Screen._vec1 = new b9.Vector3D();
+b9.Screen._mat1 = new b9.Matrix3D();
