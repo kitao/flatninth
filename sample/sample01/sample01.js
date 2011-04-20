a3bfdecbb81744fa80ff4c27b4e3570beb2d7ee9
takashi.kitao@gmail.com <takashi.kitao@gmail.com@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2011-04-20 05:03:05 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@726 7bb76224-d653-11de-9196-2db6e9c7c7ae

--- sample/sample01/sample01.js
+++ sample/sample01/sample01.js
@@ -32,7 +32,7 @@ Sample.prototype.initialize = function(x, y, z) {
 
     //this._primBuf = new b9.PrimitiveBuffer(3, 3);
     this._primBuf = new b9.PrimitiveBuffer(3);
-    this._prim = new b9.Primitive(this._primBuf);
+    this._prim = new b9.Primitive(b9.PrimitiveMode.TRIANGLES, this._primBuf);
 
     this._primBuf.setPos(0, vec.set(0.0, 50.0, 0.0));
     this._primBuf.setPos(1, vec.set(-200.0, -100.0, 0.0));
@@ -55,7 +55,7 @@ Sample.prototype.initialize = function(x, y, z) {
 
     b9.Preset.rootNode3D.addChildLast(this._prim);
 
-    this._prim2 = new b9.Primitive(this._primBuf);
+    this._prim2 = new b9.Primitive(b9.PrimitiveMode.TRIANGLES, this._primBuf);
     this._prim2.local.translate(100.0, 50.0, 10.0);
     this._prim.addChildLast(this._prim2);
 
