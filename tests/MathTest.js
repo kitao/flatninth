fc2b44670679f337ba52e707968b6c41e9a2de0e
takashi.kitao@gmail.com <takashi.kitao@gmail.com@7bb76224-d653-11de-9196-2db6e9c7c7ae>
2011-04-25 10:11:53 +0000

    git-svn-id: https://flatninth.googlecode.com/svn/trunk@743 7bb76224-d653-11de-9196-2db6e9c7c7ae

---
+++ tests/MathTest.js
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2009-2011 Takashi Kitao
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+function testMath() {
+    var i, x;
+
+    /* floor */
+    assertEquals(1.0, b9.Math.floor(1.234));
+    assertEquals(-2.0, b9.Math.floor(-1.234));
+
+    /* abs */
+    assertEquals(1.234, b9.Math.abs(1.234));
+    assertEquals(1.234, b9.Math.abs(-1.234));
+
+    /* min */
+    assertEquals(1.0, b9.Math.min(1.0, 2.0));
+    assertEquals(1.0, b9.Math.min(2.0, 1.0));
+
+    /* max */
+    assertEquals(2.0, b9.Math.max(1.0, 2.0));
+    assertEquals(2.0, b9.Math.max(2.0, 1.0));
+
+    /* clamp */
+    assertEquals(1.0, b9.Math.clamp(0.0, 1.0, 2.0));
+    assertEquals(1.5, b9.Math.clamp(1.5, 1.0, 2.0));
+    assertEquals(2.0, b9.Math.clamp(3.0, 1.0, 2.0));
+
+    /* sqrt */
+    assertEquals_float(0.0, b9.Math.sqrt(0.0));
+    assertEquals_float(2.0, b9.Math.sqrt(4.0));
+
+    /* sin_float */
+    assertEquals_float(0.0, b9.Math.sin_float(0.0));
+    assertEquals_float(1.0, b9.Math.sin_float(90.0));
+    assertEquals_float(-1.0, b9.Math.sin_float(630.0));
+    assertEquals_float(-1.0, b9.Math.sin_float(-90.0));
+    assertEquals_float(1.0, b9.Math.sin_float(-630.0));
+
+    /* cos_float */
+    assertEquals_float(0.0, b9.Math.cos_float(90.0));
+    assertEquals_float(-1.0, b9.Math.cos_float(180.0));
+    assertEquals_float(1.0, b9.Math.cos_float(720.0));
+    assertEquals_float(-1.0, b9.Math.cos_float(-180.0));
+    assertEquals_float(1.0, b9.Math.cos_float(-720.0));
+
+    /* sin_int */
+    assertEquals_float(0.0, b9.Math.sin_int(0.0));
+    assertEquals_float(1.0, b9.Math.sin_int(90.0));
+    assertEquals_float(-1.0, b9.Math.sin_int(630.0));
+    assertEquals_float(-1.0, b9.Math.sin_int(-90.0));
+    assertEquals_float(1.0, b9.Math.sin_int(-630.0));
+
+    /* cos_int */
+    assertEquals_float(0.0, b9.Math.cos_int(90.0));
+    assertEquals_float(-1.0, b9.Math.cos_int(180.0));
+    assertEquals_float(1.0, b9.Math.cos_int(720.0));
+    assertEquals_float(-1.0, b9.Math.cos_int(-180.0));
+    assertEquals_float(1.0, b9.Math.cos_int(-720.0));
+
+    /* asin */
+    assertEquals_float(0.0, b9.Math.asin(0.0));
+    assertEquals_float(90.0, b9.Math.asin(1.0));
+    assertEquals_float(-90.0, b9.Math.asin(-1.0));
+
+    /* acos */
+    assertEquals_float(90.0, b9.Math.acos(0.0));
+    assertEquals_float(0.0, b9.Math.acos(1.0));
+    assertEquals_float(180.0, b9.Math.acos(-1.0));
+
+    /* atan2 */
+    assertEquals_float(0.0, b9.Math.atan2(0.0, 1.0));
+    assertEquals_float(90.0, b9.Math.atan2(2.0, 0.0));
+    assertEquals_float(180.0, b9.Math.atan2(0.0, -3.0));
+    assertEquals_float(-90.0, b9.Math.atan2(-4.0, 0.0));
+
+    /* random_int */
+    for (i = 0; i < 10; i++) {
+        x = b9.Math.random_int(-1, 1);
+        assertTrue(x === -1 || x === 0 || x === 1);
+    }
+
+    for (i = 0; i < 10; i++) {
+        x = b9.Math.random_int(3, 5);
+        assertTrue(x === 3 || x === 4 || x === 5);
+    }
+
+    for (i = 0; i < 10; i++) {
+        x = b9.Math.random_int(-3, -5);
+        assertTrue(x === -3 || x === -4 || x === -5);
+    }
+
+    /* random_float */
+    for (i = 0; i < 10; i++) {
+        x = b9.Math.random_float(-0.5, 0.5, 0.5);
+        assertTrue(x === -0.5 || x === 0.0 || x === 0.5);
+    }
+
+    for (i = 0; i < 10; i++) {
+        x = b9.Math.random_float(3.5, 4.5, 0.5);
+        assertTrue(x === 3.5 || x === 4.0 || x === 4.5);
+    }
+
+    for (i = 0; i < 10; i++) {
+        x = b9.Math.random_float(-3.5, -4.5, -0.5);
+        assertTrue(x === -3.5 || x === -4.0 || x === -4.5);
+    }
+
+    /* lerp */
+    assertEquals_float(0.0, b9.Math.lerp(0.0, 2.0, -1.0));
+    assertEquals_float(1.0, b9.Math.lerp(0.0, 2.0, 0.5));
+    assertEquals_float(2.0, b9.Math.lerp(0.0, 2.0, 2.0));
+
+    /* equals_float */
+    assertTrue(b9.Math.equals_float(1.0, 1.0 + b9.Math.EPSILON * 0.99));
+    assertTrue(b9.Math.equals_float(1.0, 1.0 - b9.Math.EPSILON * 0.99));
+    assertFalse(b9.Math.equals_float(1.1, 2.0));
+    assertFalse(b9.Math.equals_float(1.0, 2.1));
+
+    /* EPSILON */
+    assertEquals(0.0001, b9.Math.EPSILON);
+
+    /* PI */
+    assertEquals_float(3.14159265358979, b9.Math.PI);
+
+    /* DEG_TO_RAD */
+    assertEquals_float(b9.Math.PI / 180.0, b9.Math.DEG_TO_RAD);
+
+    /* RAD_TO_DEG */
+    assertEquals_float(180.0 / b9.Math.PI, b9.Math.RAD_TO_DEG);
+}
---
+++ tests/b9Test.js
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2009-2011 Takashi Kitao
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+function testB9() {
+    var ctorCount1, ctorCount2, ctorCount3;
+    var dtorCount1, dtorCount2, dtorCount3;
+    var TestClass0, TestClass1, TestClass2, TestClass3;
+    var testIns0, testIns1, testIns2, testIns3;
+
+    /* VERSION */
+    assertEquals(0.10, b9.VERSION);
+
+    /* createClass */
+    TestClass0 = b9.createClass();
+
+    ctorCount1 = 0;
+    dtorCount1 = 0;
+    TestClass1 = b9.createClass();
+    TestClass1.prototype.initialize = function(param) { ctorCount1++; this.param = param; };
+    TestClass1.prototype.finalize = function() { dtorCount1++; };
+
+    ctorCount2 = 0;
+    dtorCount2 = 0;
+    TestClass2 = b9.createClass(TestClass1);
+    TestClass2.prototype.initialize = function() { ctorCount2++; this.initializeSuper(456); };
+    TestClass2.prototype.finalize = function() { dtorCount2++; this.finalizeSuper(); };
+
+    ctorCount3 = 0;
+    dtorCount3 = 0;
+    TestClass3 = b9.createClass(TestClass2);
+    TestClass3.prototype.initialize = function() { ctorCount3++; this.initializeSuper(); };
+    TestClass3.prototype.finalize = function() { dtorCount3++; this.finalizeSuper(); };
+
+    testIns0 = new TestClass0();
+
+    testIns1 = new TestClass1(123);
+
+    assertUndefined(testIns1.initializeSuper);
+    assertUndefined(testIns1.finalizeSuper);
+    assertEquals(1, ctorCount1);
+    assertEquals(0, dtorCount1);
+    assertEquals(0, ctorCount2);
+    assertEquals(0, dtorCount2);
+    assertEquals(0, ctorCount3);
+    assertEquals(0, dtorCount3);
+    assertEquals(123, testIns1.param);
+
+    testIns1.finalize();
+
+    assertEquals(1, ctorCount1);
+    assertEquals(1, dtorCount1);
+    assertEquals(0, ctorCount2);
+    assertEquals(0, dtorCount2);
+    assertEquals(0, ctorCount3);
+    assertEquals(0, dtorCount3);
+
+    testIns2 = new TestClass2();
+
+    assertNotUndefined(testIns2.initializeSuper);
+    assertNotUndefined(testIns2.finalizeSuper);
+    assertEquals(2, ctorCount1);
+    assertEquals(1, dtorCount1);
+    assertEquals(1, ctorCount2);
+    assertEquals(0, dtorCount2);
+    assertEquals(0, ctorCount3);
+    assertEquals(0, dtorCount3);
+    assertEquals(456, testIns2.param);
+
+    testIns2.finalize();
+
+    assertEquals(2, ctorCount1);
+    assertEquals(2, dtorCount1);
+    assertEquals(1, ctorCount2);
+    assertEquals(1, dtorCount2);
+    assertEquals(0, ctorCount3);
+    assertEquals(0, dtorCount3);
+
+    testIns3 = new TestClass3();
+
+    assertNotUndefined(testIns3.initializeSuper);
+    assertNotUndefined(testIns3.finalizeSuper);
+    assertEquals(3, ctorCount1);
+    assertEquals(2, dtorCount1);
+    assertEquals(2, ctorCount2);
+    assertEquals(1, dtorCount2);
+    assertEquals(1, ctorCount3);
+    assertEquals(0, dtorCount3);
+    assertEquals(456, testIns3.param);
+
+    testIns3.finalize();
+
+    assertEquals(3, ctorCount1);
+    assertEquals(3, dtorCount1);
+    assertEquals(2, ctorCount2);
+    assertEquals(2, dtorCount2);
+    assertEquals(1, ctorCount3);
+    assertEquals(1, dtorCount3);
+}
